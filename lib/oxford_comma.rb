04788e47ea40b0e452a501f8eae6f657e01fcc63
Vicki Hamm <vicki.cello@gmail.com>
2018-06-21 13:27:31 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
 def oxford_comma(array)
-  array = [].join(" ,")
+  if array.size < 3
+    array.join(" and ")
+  else
+    array[0, array.length - 1].join(", ") + ", and " + array.last
+  end
 end
+
